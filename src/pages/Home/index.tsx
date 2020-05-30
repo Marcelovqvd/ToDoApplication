bb380c18d2ddb5f504644bdc3b8dcec5d490372c
Marcelo <marcelovqvd@gmail.com>
2020-05-30 12:41:14 -0300

    styled components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import { Title } from './styles';
+
 const Home: React.FunctionComponent = () => {
-  return <h1>Home</h1>;
+  return <Title>Home</Title>;
 }
 
 export default Home;
